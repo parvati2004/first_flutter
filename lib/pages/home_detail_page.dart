e9133e6fd497e348a59f99d3763072a936a2f4cc
Nikhil Shinde <parvatisaud774@gmail.com>
2025-07-10 09:29:45 +0530

    day18 completed

--- lib/pages/home_detail_page.dart
+++ lib/pages/home_detail_page.dart
@@ -10,7 +10,6 @@ class HomeDetailPage extends StatelessWidget {
   const HomeDetailPage({
     Key? key,
     required this.catalog,
-  // ignore: unnecessary_null_comparison
   })  : assert(catalog != null),
         super(key: key);
 
@@ -18,25 +17,55 @@ class HomeDetailPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      backgroundColor:MyTheme.creamColor,
-      body:SafeArea(
+      backgroundColor: MyTheme.creamColor,
+      bottomNavigationBar:   
+       Container(
+        color:Colors.white,
+        child: ButtonBar(
+                    alignment: MainAxisAlignment.spaceBetween,
+                    buttonPadding: EdgeInsets.zero,
+                    children: [
+                      "₹${catalog.price}".text.xl4.red800.make(),
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(MyTheme.darkBluishColor),
+                          shape: MaterialStateProperty.all(const StadiumBorder()),
+                        ),
+                        child: "Buy".text.xl.make(),
+                      ).wh(100,50),
+                    ],
+                  ),
+      ),
+      body: SafeArea(
+        bottom:false,
         child: Column(
           children: [
             Hero(
-               tag:Key(catalog.id.toString()),
+              tag: Key(catalog.id.toString()),
               child: Image.network(catalog.image),
-              ).p32(),
-
-              Expanded(child:VxArc(
-                height:30.0,
-                arcType:VxArcType.convey,
+            ).h32(context),
+            Expanded(
+              child: VxArc(
+                height: 30.0,
+                arcType: VxArcType.convey,
+                edge: VxEdge.top,
                 child: Container(
-                  color: Colors.red,
+                  color: Colors.white,
+                  width: context.screenWidth,
+                  child: Column(
+                    children: [
+                      catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+                      catalog.desc.text.textStyle(context.captionStyle).xl.make(),
+                      10.heightBox,
+                    ],
+                  ).py64(),
                 ),
-              ))
+              ),
+            ),
           ],
-        )
-      )
+        ),
+      ),
     );
   }
 }
